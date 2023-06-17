ce1cb05f0989f7108b0983ed1347dda4f7ca33bd
Natasha <111096866+Tashatoon@users.noreply.github.com>
2023-06-17 11:48:32 -0400

    javascript

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("light-mode");
 }
+
+const toggleMenu = () => {
+      const menu = document.getElementById('menu');
+      menu.classList.toggle('show');
+    };
+
+    // Example: Dynamically update content based on screen size
+    const updateContent = () => {
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth < 600) {
+        document.getElementById('content').textContent = 'Small Screen Content';
+      } else {
+        document.getElementById('content').textContent = 'Large Screen Content';
+      }
+    };
+
+    // Event listener for window resize
+    window.addEventListener('resize', updateContent);
