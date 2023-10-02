6fd4a98596c5f5dc482c3a252bace9a97c8265fe
Natasha <111096866+Tashatoon@users.noreply.github.com>
2023-10-02 18:31:56 -0400

    image update/slideshow

--- script.js
+++ script.js
@@ -19,5 +19,19 @@ const toggleMenu = () => {
       }
     };
 
+    var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); 
+}
     // Event listener for window resize
     window.addEventListener('resize', updateContent);
